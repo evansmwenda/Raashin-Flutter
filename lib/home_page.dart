5ba49a37941fbbb940ff363801934c203bcd615d
evansmwenda <evansmwenda.em@gmail.com>
2019-11-20 17:46:46 +0300

    added navigation drawer and settings route

---
+++ lib/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'screens/settings_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var aboutChild = AboutListTile(
+        child: Text("About"),
+        applicationName: "Application Name",
+        applicationVersion: "v1.0.0",
+        applicationIcon: Icon(Icons.adb),
+        icon: Icon(Icons.info));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raashin"),
+      ),
+      body: Center(child: Text('My Home Page!')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          //remove any padding
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("My Drawer"),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text("Home"),
+              leading: Icon(Icons.home),
+              onTap: () {
+                //update the state of the app
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Settings"),
+              leading: Icon(Icons.settings),
+              onTap: () {
+                //update the state of the app
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Account"),
+              leading: Icon(Icons.person),
+              onTap: () {
+                //update the state of the app
+                //and close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Orders"),
+              leading: Icon(Icons.card_giftcard),
+              onTap: () {
+                //update the state of the app
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Categories"),
+              leading: Icon(Icons.format_list_bulleted),
+              onTap: () {
+                //update the state of the app
+                Navigator.pop(context);
+              },
+            ),
+            aboutChild,
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'dart:async';
 
 ///coded by 0utlaw
 ///on Nov 2019
 ///
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: MyApp(),
-    )
-  );
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "hello Raashin",
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(
+          size: 400,
+        ),
       ),
     );
   }
-
 }
